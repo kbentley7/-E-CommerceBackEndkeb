ad14da0e6ee2aaebf3deabdf858f771359b851c3
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-14 18:29:17 -0400

    edited models

--- models/Category.js
+++ models/Category.js
@@ -6,19 +6,20 @@ class Category extends Model {}
 
 Category.init(
   {
-    id :
-    {
+    // define columns
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
-    category_name:
-    {
+    category_name: {
       type: DataTypes.STRING,
       allowNull: false
     }
+    
   },
+
   {
     sequelize,
     timestamps: false,
