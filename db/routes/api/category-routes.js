331dc8ab9b852d2774337004465223378bc0ddb9
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-15 11:29:47 -0400

    edited models

--- db/routes/api/category-routes.js
+++ db/routes/api/category-routes.js
@@ -54,7 +54,20 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categoryData => {
+      if (!categoryData) {
+        res.status(404).json({ message: 'No Category found with that ID.' });
+        return;
+      }
 });
 
 router.delete('/:id', (req, res) => {
